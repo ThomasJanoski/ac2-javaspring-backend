030f0d83f6e5818e4036fcf1e63c0137edf8503e
ThomasJanoski <thomassilveira2020@gmail.com>
2025-10-29 21:50:42 -0300

    Deletando SuperssWarning, depuração

--- src/main/java/com/facens/ac2/Repositories/SetorRepository.java
+++ src/main/java/com/facens/ac2/Repositories/SetorRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import com.facens.ac2.Models.Setor;
@@ -18,9 +19,9 @@ public interface SetorRepository extends JpaRepository<Setor, Integer> {
     @Query("SELECT s FROM Setor s WHERE s.nome = :nome")
     Optional<Setor> findByNome(@Param("nome") String nome);
 
-    @SuppressWarnings("null")
     @Query("SELECT s FROM Setor s WHERE s.id = :id")
-    Optional<Setor> findById(@Param("id") Integer id);
+    @NonNull
+    Optional<Setor> findById(@Param("id") @NonNull Integer id);
 
     @Query("SELECT s FROM Setor s LEFT JOIN FETCH s.funcionarios WHERE s.id = :id")
     Setor findByIdWithFuncionarios(@Param("id") Integer id);
